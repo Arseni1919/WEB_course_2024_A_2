5709a747ac077857d98901e934cfb9c512ef591a
perchik_air <1919ars@gmail.com>
2024-02-21 18:39:33 +0200

    more

--- app.py
+++ app.py
@@ -2,8 +2,11 @@
 from flask import redirect
 from flask import Flask, redirect, url_for
 from flask import render_template
+from flask import request, session
+
 
 app = Flask(__name__)
+app.secret_key = '123'
 
 
 # @app.route('/profile')
@@ -33,6 +36,38 @@ def profile_page():
 @app.route('/about')
 def about_page():
     return render_template('about.html')
+
+
+@app.route('/logout', methods=['GET'])
+def logout_func():
+    session['logged_in'] = False
+    session['username'] = ''
+    session['email'] = ''
+    return redirect(url_for('login_func'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_func():
+    if request.method == 'GET':
+        if 'username' in request.args:
+            username = request.args['username']
+            email = request.args['email']
+            password = request.args['password']
+            # do the check with DB
+            return render_template('login.html',
+                                   username=username, email=email)
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        # do the check with DB
+        session['username'] = username
+        session['email'] = email
+        session['logged_in'] = True
+        return render_template('login.html',
+                                username=username, email=email)
+
+    return render_template('login.html')
 
 
 
