f9c06526332a9c0f02d01f65d4f608a9c1cfaae3
perchik_air <1919ars@gmail.com>
2024-02-28 18:23:06 +0200

    more

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import redirect
 from flask import Flask, redirect, url_for
 from flask import render_template
 from flask import request, session
+from flask import jsonify
 
 
 app = Flask(__name__)
@@ -32,6 +33,25 @@ def profile_page():
     # return 'Welcome to profile page!'
     return redirect(url_for('home_page'))
 
+
+@app.route('/users', defaults={'user_id': 123})
+@app.route('/users/<int:user_id>/products/<product_name>')
+def users_func(user_id, product_name):
+    # DB ...
+    # v
+    return render_template('users.html', user_name=user_id, product_name=product_name)
+
+
+@app.route('/api/orders/<int:order_id>')
+def orders_func(order_id):
+    # DB ...
+    order_data = {
+        'name': 'computer',
+        'date': 2014,
+        'my_id': order_id,
+        'description': 'good'
+    }
+    return jsonify(order_data)
 
 @app.route('/about')
 def about_page():
